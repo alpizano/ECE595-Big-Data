a11dddfebde998fbb35bd71beda09d61d41c64af
ennth <39110859+aemaeth0@users.noreply.github.com>
2019-10-13 03:02:27 -0500

    added if/else for when money can't be withdrawn and images of code output

--- Hands-on/Task 1/atm.py
+++ Hands-on/Task 1/atm.py
@@ -1,10 +1,10 @@
 amount = int(input("Enter the amount for withdrawal: "))
 
-print(amount)
-
 bill_100 = 100
 bill_20 = 20
 bill_5 = 5
+
+original_amount = amount
 
 count_100 = amount/bill_100
 amount = amount % bill_100
@@ -15,7 +15,10 @@ amount = amount % bill_20
 count_5 = amount/bill_5
 amount = amount % bill_5
 
-print("Please collect your bills as follows:")
-print("$%i: %i" % (bill_100,count_100))
-print("$%i: %i" % (bill_20,count_20))
-print("$%i: %i" % (bill_5,count_5))
+if 100*int(count_100) + 20*int(count_20) + 5*int(count_5) == original_amount:
+    print("Please collect your bills as follows:")
+    print("$%i: %i" % (bill_100,count_100))
+    print("$%i: %i" % (bill_20,count_20))
+    print("$%i: %i" % (bill_5,count_5))
+else:
+    print("The amount cannot be withdrawn")
